ab196c4b5ff9caff1a823985a9b5729c925b53c2
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-07-26 23:24:21 -0500

    Completed React components for full functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-import ContactList from "./components/ContactList"
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null)
+;  
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <SelectedContact
+        selectedContactId={selectedContactId}
+        setSelectedContactId={setSelectedContactId} />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
-  )
+  );
 }
 
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,17 +1,12 @@
-import React from "react";
 import { useState } from 'react';
 import { useEffect } from 'react';
 import ContactRow from "./ContactRow";
 
 
-const dummyContacts = [
-    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
+export default function ContactList({ setSelectedContactId }) {
+    const [contacts, setContacts] = useState([]);
+    const [error, setError] = useState(null);
 
-export default function ContactList() {
-    const [contacts, setContacts] = useState(dummyContacts);
     useEffect(() => {
         async function fetchContacts() {
             try {
@@ -19,7 +14,7 @@ export default function ContactList() {
                 const result = await response.json();
                 setContacts(result);
             } catch (error) {
-                console.error(error);
+                setError(error);
             }
         }
         fetchContacts()
@@ -38,10 +33,17 @@ return (
                 <td>Email</td>
                 <td>Phone</td>
             </tr>
-            {contacts.map((contact) => {
-                    return <ContactRow key={contact.id} contact={contact}
-                    />;
-                })}
+            {error ? (
+                <tr>{error}</tr>
+            ) : (
+            contacts.map((contact) => {
+                    return (
+                     <ContactRow key={contact.id} contact={contact}
+                    setSelectedContactId={setSelectedContactId}
+                    />
+                    );
+                })
+            )}
         </tbody>
     </table>
 );
